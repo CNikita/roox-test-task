3234516df31d4c7164ae94bd36c08f37d6cf22db
Nikita Gainanov <slesher35@gmail.com>
2022-03-25 23:55:23 +0300

    Updates

--- src/Components/Button/index.js
+++
@@ -1,8 +0,0 @@
-import "./style.scss";
-
-export default function Button({ buttonName, handler }) {
-    return (
-        <button type="button" onClick={() => handler(buttonName)}>{buttonName}</button>
-    )
-};
-
--- src/Components/ListUsers/ListUsersContainer.js
+++
@@ -1,9 +0,0 @@
-import ListUsersComponent from "./ListUsersComponent";
-import CartUser from "../CartUser";
-
-export default function ListUsersContainer({ users }) {
-    const userItems = users.map(item => <CartUser key={item.id} item={item} />);
-    return (
-        <ListUsersComponent userItems={userItems} />
-    )
-};
